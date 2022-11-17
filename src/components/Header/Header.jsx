16cd4f1f1ccfd723714be3a784f80074742c087e
JustinLung <justinlung77@gmail.com>
2022-11-17 12:45:14 +0100

    hamburger menu made

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,11 @@ export default function Header() {
       <a href="/">
         <img src="/assets/images/bold-lynk-logo.svg" alt="Logo" />
       </a>
+      <button className={styles.hamburger}>
+        <span className={styles.hamburger__line} />
+        <span className={styles.hamburger__line} />
+        <span className={styles.hamburger__line} />
+      </button>
     </header>
   );
 }
