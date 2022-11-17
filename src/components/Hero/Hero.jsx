f130add964451acb667289b0d671cdf8e832bd2a
JustinLung <justinlung77@gmail.com>
2022-11-17 14:49:18 +0100

    added gradient

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero({ heroTitle, heroDescription }) {
       <section className={styles.hero}>
         <h1 className={styles.hero__title}>{heroTitle}</h1>
         <p className={styles.hero__description}>{heroDescription}</p>
-        {/* <div className={styles.hero__waves}></div> */}
+        <div className={styles.hero__waves}></div>
       </section>
     </>
   );
